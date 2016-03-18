6943d7b239f21ff1a202888ec91429ff93b8502a
skuirrel0311 <rureikus7223@gmail.com>
2016-03-18 18:18:12 +0900

    キャラクタ移動＋カメラ移動＋カメラ回転が完成したのでくっ付けてみた。 キーボードの”X”を押すとその場にInstanceAnchorを生成できるようにした。

---
+++ Tramp/Assets/Game/Camera/Camera.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Camera : MonoBehaviour
+{
+    public float rotationSpeed = 5;
+    private float rotationY;
+
+    private Vector3 oldPlayerPosition;
+    private GameObject player;
+
+    void Start()
+    {
+        rotationY = 0;
+        player = GameObject.Find("Player");
+        oldPlayerPosition = player.transform.position;
+    }
+
+    void Update()
+    {
+        float mouseX = Input.GetAxis("Mouse X");
+
+        rotationY += mouseX * rotationSpeed;
+
+        transform.rotation = Quaternion.Euler(0, rotationY, 0);
+
+        Vector3 movement = player.transform.position - oldPlayerPosition;
+
+        //プレイヤーが移動していなかったら終了
+        if (movement.magnitude == 0) return;
+
+        //プレイヤーについていく
+        transform.position += movement;
+
+        oldPlayerPosition = player.transform.position;
+    }
+}
---
+++ Tramp/Assets/Game/Player/Player.cs
@@ -0,0 +1,57 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Player : MonoBehaviour
+{
+    public float moveSpeed = 5;
+    public float rotationSpeed = 360;
+    public GameObject instanceAnchor;
+
+    private Animator animator;
+    private GameObject camera;
+
+    void Start()
+    {
+        animator = GetComponentInChildren<Animator>();
+        camera = GameObject.Find("Camera");
+    }
+
+    void Update()
+    {
+        Move();
+
+        if (Input.GetKeyDown(KeyCode.X))
+        {
+            CreateAnchor();
+        }
+    }
+
+    void Move()
+    {
+        Vector3 direction = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical"));
+
+        //入力の角度をカメラの角度に曲げる
+        direction = camera.transform.rotation * direction;
+
+        animator.SetFloat("Speed", direction.magnitude);
+
+        //移動していなかったら終了
+        if (direction == Vector3.zero) return;
+
+        //弧を描くように移動
+        Vector3 forward = Vector3.Slerp(
+            transform.forward,  //正面から
+            direction,          //入力の角度まで
+            rotationSpeed * Time.deltaTime / Vector3.Angle(transform.forward, direction)
+            );
+        //向きを変える
+        transform.LookAt(transform.position + forward);
+
+        transform.position += direction * moveSpeed * Time.deltaTime;
+    }
+
+    void CreateAnchor()
+    {
+        Instantiate(instanceAnchor, transform.position, transform.rotation);
+    }
+}
