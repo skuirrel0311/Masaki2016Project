60e3733768a6ab2398da0dc56c153c1b60cef164
TachibanaKoki <standchat0820@gmail.com>
2016-03-23 15:54:48 +0900

    【Shot】着弾予想地点に向けて照準が出るようにしました。R,Fkeyで照準が上下

---
+++ Tramp/Assets/Game/Player/Ammo/AlignmentLine.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+
+/// <summary>
+/// 照準の線の描画Nozzleにアタッチする
+/// </summary>
+public class AlignmentLine : MonoBehaviour {
+
+    LineRenderer lineRenderer;
+
+	// Use this for initialization
+	void Start ()
+    {
+        lineRenderer = GetComponent<LineRenderer>();
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        RaycastHit hit;
+        lineRenderer.SetPosition(0,transform.position);
+
+        //直線上にオブジェクトがあればそこまでのラインを描画させる
+        if (Physics.Raycast(transform.position,transform.forward, out hit))
+        {
+            Transform objectHit = hit.transform;
+            lineRenderer.SetPosition(1,hit.point);
+        }
+        else
+        {
+            lineRenderer.SetPosition(1,transform.position+(transform.forward*100));
+        }
+    }
+}
--- Tramp/Assets/Game/Player/PlayerShot.cs
+++ Tramp/Assets/Game/Player/PlayerShot.cs
@@ -6,16 +6,34 @@ public class PlayerShot : MonoBehaviour {
     [SerializeField]
     GameObject Ammo;
 
-	// Use this for initialization
-	void Start () {
-	
+    [SerializeField]
+    GameObject Nozzle;
+
+    // Use this for initialization
+    void Start ()
+    {
+
 	}
 	
 	// Update is called once per frame
-	void Update () {
+	void Update ()
+    {
+        float vertical=0;
+        //vertical = Input.GetAxis("Vertical");
+        if (Input.GetKey(KeyCode.R))
+        {
+            vertical = 1.0f;
+        }
+        else  if(Input.GetKey(KeyCode.F))
+        {
+            vertical = -1.0f;
+        }
+
+        Nozzle.transform.Rotate(vertical,0,0);
+
         if (Input.GetButtonDown("Fire2"))
         {
-            Instantiate(Ammo,transform.position+Vector3.up,transform.rotation);
+            Instantiate(Ammo,transform.position+Vector3.up,Nozzle.transform.rotation);
         }
 	}
 }
