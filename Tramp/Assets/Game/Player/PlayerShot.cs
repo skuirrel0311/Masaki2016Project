ceb96af79841b9f4bd3780533ffa6c2d23abbd7a
TachibanaKoki <standchat0820@gmail.com>
2016-03-23 17:08:31 +0900

    【Shot】照準を右スティックで移動できるように変更。照準の移動できる範囲に制限をつけた。

--- Tramp/Assets/Game/Player/PlayerShot.cs
+++ Tramp/Assets/Game/Player/PlayerShot.cs
@@ -9,31 +9,53 @@ public class PlayerShot : MonoBehaviour {
     [SerializeField]
     GameObject Nozzle;
 
+    [SerializeField]
+    float MaxAngle = 30;
+
+    private Vector3 NozzleAngle;
+
     // Use this for initialization
     void Start ()
     {
-
+        NozzleAngle = Vector3.zero;
 	}
 	
 	// Update is called once per frame
 	void Update ()
     {
         float vertical=0;
-        //vertical = Input.GetAxis("Vertical");
+        float horizontal=0;
+
         if (Input.GetKey(KeyCode.R))
         {
             vertical = 1.0f;
         }
-        else  if(Input.GetKey(KeyCode.F))
+        else if (Input.GetKey(KeyCode.F))
         {
             vertical = -1.0f;
         }
 
-        Nozzle.transform.Rotate(vertical,0,0);
+        vertical = Input.GetAxis("Vertical2");
+        horizontal = Input.GetAxis("Horizontal2");
+
+        NozzleAngle += new Vector3(vertical,horizontal,0);
+        //制限内であれば照準を自由に移動
+        if (NozzleAngle.magnitude < MaxAngle)
+        {
+            Nozzle.transform.Rotate(vertical, horizontal, 0);
+        }
+        //制限に引っかかれば制限する
+        else
+        {
+            NozzleAngle = NozzleAngle.normalized * MaxAngle;
+            Nozzle.transform.localEulerAngles = NozzleAngle;
+        }
 
         if (Input.GetButtonDown("Fire2"))
         {
-            Instantiate(Ammo,transform.position+Vector3.up,Nozzle.transform.rotation);
+            Instantiate(Ammo,Nozzle.transform.position,Nozzle.transform.rotation);
         }
 	}
+
+
 }
