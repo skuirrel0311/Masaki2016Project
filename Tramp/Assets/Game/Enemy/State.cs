eccbea36d2b45834737ada7aaea457a706f8539e
skuirrel <rureikus7223@gmail.com>
2016-03-28 16:31:57 +0900

    【Enemy】型だけ作った。

---
+++ Tramp/Assets/Game/Enemy/State.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class State<T>
+{
+    protected T owner;
+
+    public State(T owner)
+    {
+        this.owner = owner;
+    }
+
+    //この状態になったときに1度だけ呼ばれる
+    public virtual void Enter() { }
+
+    //この状態のとき毎フレーム呼ばれる
+    public virtual void Execute() { }
+
+    //この状態から別のステートになったときに1度だけ呼ばれる
+    public virtual void Exit() { }
+}
