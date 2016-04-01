03cd0c86be0a27317d7bea5a521cae249f8a4548
TachibanaKoki <standchat0820@gmail.com>
2016-04-01 20:43:25 +0900

    【Camera】キーボードでRkeyを押すと近くのアンカーにカメラが向く（上下未実装）その状態でDkeyを押すとそのアンカーが消える

--- Tramp/Assets/Game/Camera/CameraControl.cs
+++ Tramp/Assets/Game/Camera/CameraControl.cs
@@ -8,6 +8,7 @@ public class CameraControl : MonoBehaviour
 
     private Vector3 oldPlayerPosition;
     private GameObject player;
+    private GameObject nearAnchor;
 
     void Start()
     {
@@ -18,12 +19,62 @@ public class CameraControl : MonoBehaviour
 
     void Update()
     {
+        PlayerTrace();
+
+        if (Input.GetKeyDown(KeyCode.R))
+        {
+            nearAnchor = GetNearAnchor();
+        }
+
+        if (Input.GetKey(KeyCode.R))
+        {
+            if (nearAnchor == null) return;
+            if (Input.GetKeyDown(KeyCode.D))
+            {
+                Destroy(nearAnchor);
+            }
+            Vector3 vec = nearAnchor.transform.position - transform.position;
+            transform.rotation = Quaternion.Euler(0,VectorToAngle(vec.x,vec.z), 0);
+            return;
+        }
+
         float mouseX = Input.GetAxis("Horizontal2");
 
         rotationY += mouseX * rotationSpeed;
 
         transform.rotation = Quaternion.Euler(0, rotationY, 0);
+    }
 
+    private float VectorToAngle(float a,float b)
+    {
+         return Mathf.Atan2(a, b) * 180 / Mathf.PI;
+    }
+
+    //todo:CreateFlowとメソッドがかぶっているのでライブラリを作成する
+    GameObject GetNearAnchor()
+    {
+        GameObject gameObject=null;
+        //一番近いアンカーを探す
+        GameObject[] objects = GameObject.FindGameObjectsWithTag("Anchor");
+        if (objects.Length <= 0) return null;
+
+        float distance = 1000000;
+
+        foreach (GameObject obj in objects)
+        {
+            if (Vector3.Distance(transform.position, obj.transform.position) < distance)
+            {
+                Vector3 vec = obj.transform.position - transform.position;
+                distance = vec.magnitude;
+                gameObject = obj;
+            }
+        }
+
+        return gameObject;
+    }
+
+    private void PlayerTrace()
+    {
         Vector3 movement = player.transform.position - oldPlayerPosition;
 
         //プレイヤーが移動していなかったら終了
