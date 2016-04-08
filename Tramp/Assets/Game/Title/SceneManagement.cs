f66dfd18f7761a1b104f92960dd951f4d594db22
TachibanaKoki <standchat0820@gmail.com>
2016-04-08 19:06:55 +0900

    【Title】タイトルシーンを作成しました。

---
+++ Tramp/Assets/Game/Title/SceneManagement.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class SceneManagement : MonoBehaviour {
+
+    AsyncOperation mainScene;
+
+	// Use this for initialization
+	void Start () {
+        //シーンを予め読み込んでおく
+        mainScene = SceneManager.LoadSceneAsync ("GPGPUtest");
+        mainScene.allowSceneActivation = false;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetButtonDown("Fire1"))
+        {
+            //シーン遷移を開始する
+            mainScene.allowSceneActivation = true;
+        }
+	}
+}
