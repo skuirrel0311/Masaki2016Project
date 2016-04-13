911df92f5b70a41f15ff9c18a78698f878dc9b06
skuirrel <rureikus7223@gmail.com>
2016-04-13 12:42:36 +0900

    【Appealitem】アピールアイテム(マイク)がスタート時にランダムな足場に配置されるようにしました。

---
+++ Tramp/Assets/Game/AppealItem/AppealItem.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class AppealItem : MonoBehaviour
+{
+    //空中の足場のリスト
+    List<Transform> scaffoldList = new List<Transform>();
+
+    void Start()
+    {
+        //生成されている足場をすべて取得する
+        GameObject stage = GameObject.Find("Stage");
+
+        foreach(Transform child in stage.transform)
+        {
+            if(child.name == "scaffold")
+            {
+                scaffoldList.Add(child);
+            }
+        }
+        SpawnInRandomPosition();
+    }
+
+    void Update()
+    {
+
+    }
+
+    /// <summary>
+    /// ランダムな足場にスポーンする。
+    /// </summary>
+    public void SpawnInRandomPosition()
+    {
+        int num = Random.Range(0, scaffoldList.Count);
+
+        transform.position = scaffoldList[num].position;
+    }
+}
