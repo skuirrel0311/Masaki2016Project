284ad2dd0c71eedf167fbcb8bff576bf4cd2d8be
Kouki <standchat0820@gmail.com>
2016-04-24 23:11:57 +0900

    【Anchor】アンカーがプレイヤーとあたった時に消えるように変更。エフェクトも追加

--- Tramp/Assets/Game/Anchor/Script/AnchorHit.cs
+++ Tramp/Assets/Game/Anchor/Script/AnchorHit.cs
@@ -7,6 +7,16 @@ public class AnchorHit : MonoBehaviour {
     //何回ヒットすれば壊れるか
     int Hp=10;
 
+    [SerializeField]
+    GameObject HitEffect;
+
+    GameObject FlowEffect;
+
+    void Start()
+    {
+        FlowEffect = transform.FindChild("FlowEffect(Clone)").gameObject;
+    }
+
     void OnCollisionEnter(Collision col)
     {
         if (col.gameObject.tag=="Ammo")
@@ -18,5 +28,17 @@ public class AnchorHit : MonoBehaviour {
                 Destroy(gameObject);
             }
         }
+        else if (col.gameObject.tag=="Player")
+        {
+            if (FlowEffect!=null)
+            {
+                FlowEffect.transform.parent = null;
+                Destroy(FlowEffect, 2);
+            }
+            Destroy(gameObject);
+
+            Instantiate(HitEffect,transform.position,Quaternion.identity);
+        }
     }
+
 }
