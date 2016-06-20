9ca9000d18fbfa948e6dbe9f9a96d08b3cafdde4
TachibanaKoki <standchat0820@gmail.com>
2016-06-20 15:57:55 +0900

    【Animation】弾を打つ時のアニメーションの同期

---
+++ Tramp/Assets/Game/Networks/AnimationSync.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine;
+using UnityEngine.Networking;
+using System.Collections;
+
+public class AnimationSync : NetworkBehaviour
+{
+
+    Animator animator;
+
+    float oldWeight;
+
+    [SyncVar]
+    float weight;
+
+    // Use this for initialization
+    void Start()
+    {
+        animator = GetComponent<Animator>();
+        weight = 0;
+        oldWeight = weight;
+    }
+
+    // Update is called once per frame
+    void LateUpdate()
+    {
+
+        if ((animator.GetLayerWeight(1) != oldWeight) && isLocalPlayer)
+        {
+            CmdSetWeight(animator.GetLayerWeight(1));
+        }
+
+        oldWeight = weight;
+        animator.SetLayerWeight(1,weight);
+    }
+
+    [ClientCallback]
+    [Command]
+    void CmdSetWeight(float w)
+    {
+        weight = w;
+    }
+}
